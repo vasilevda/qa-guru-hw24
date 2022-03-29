9d9e45fc726baab494b1fcaaf6dee2a5402ffd4f
da.vasilyev <dmitry.vasilyew@gmail.com>
2022-03-29 20:57:45 +0300

    Добавлена инфраструктура для тестов

---
+++ src/test/java/helpers/ServerConfig.java
@@ -0,0 +1,18 @@
+package helpers;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config/ServerConfig.properties")
+public interface ServerConfig extends Config {
+    @Key("https.server.host")
+    @DefaultValue("https://${server.host.login}:${server.host.password}@selenoid.autotests.cloud/wd/hub")
+    String hostname();
+
+    @Key("server.host.login")
+    @DefaultValue("user1")
+    String login();
+
+    @Key("server.host.password")
+    @DefaultValue("1234")
+    String password();
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attachments;
+import helpers.ServerConfig;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+
+        Configuration.browserSize = "1920x1080";
+//        Configuration.baseUrl = "https://github.com";
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.remote = cfg.hostname();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+        closeWebDriver();
+    }
+}
+
